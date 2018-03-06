8f7f8ff10d43fcd44944467c4442ea458b1e7eb1
nnelson16 <nn167@scarletmail.rutgers.edu>
2018-03-05 23:00:55 -0500

    ufuncs

---
+++ numpytutorials3.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+x = np.arange(9).reshape((3, 3))
+print(2 ** x)
+
+x = np.arange(4)
+print("x     =", x)
+print("x + 5 =", x + 5)
+print("x - 5 =", x - 5)
+print("x * 2 =", x * 2)
+print("x / 2 =", x / 2)
+print("x // 2 =", x // 2)  # floor division
+
+print("-x     = ", -x)
+print("x ** 2 = ", x ** 2)
+print("x % 2  = ", x % 2)
+
+x = np.array([-2, -1, 0, 1, 2])
+print(abs(x))
+
+theta = np.linspace(0, np.pi, 3)
+print("theta      = ", theta)
+print("sin(theta) = ", np.sin(theta))
+print("cos(theta) = ", np.cos(theta))
+print("tan(theta) = ", np.tan(theta))
+
+x = [-1, 0, 1]
+print("x         = ", x)
+print("arcsin(x) = ", np.arcsin(x))
+print("arccos(x) = ", np.arccos(x))
+print("arctan(x) = ", np.arctan(x))
+
+x = [1, 2, 3]
+print("x     =", x)
+print("e^x   =", np.exp(x))
+print("2^x   =", np.exp2(x))
+print("3^x   =", np.power(3, x))
+
+x = [1, 2, 4, 10]
+print("x        =", x)
+print("ln(x)    =", np.log(x))
+print("log2(x)  =", np.log2(x))
+print("log10(x) =", np.log10(x))
